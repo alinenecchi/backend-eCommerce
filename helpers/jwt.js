da49d410b39b452cf736d01aaa7d011a4c631530
aline-necchi <alinenecchi@gmail.com>
2022-12-02 22:34:17 -0300

    upadate

---
+++ helpers/jwt.js
@@ -0,0 +1,32 @@
+const expressJwt = require("express-jwt");
+
+function authJwt() {
+  const secret = process.env.JWT_KEY;
+  const api = process.env.API_URL;
+  return expressJwt({
+    secret,
+    algorithms: ["HS256"],
+    userProperty: "auth",
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+      // { url: /(.*)/ },
+    ],
+  });
+}
+
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
+
+  done();
+}
+
+module.exports = authJwt;
